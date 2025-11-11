153d7f50433e923b85187aa8c5d64ab2ef824fa6
Sandyhlatshwayo <sandyhlatshwayo8@gmail.com>
2025-11-11 02:47:52 +0200

    Final Stdent Progress

--- M4-Website/Instructor/StudentProgress.ascx.cs
+++ M4-Website/Instructor/StudentProgress.ascx.cs
@@ -112,6 +112,7 @@ namespace M4_Website
 
             AddCommentToStudentProgress(studentID, newComment);
             TextBox1.Text = "";
+            GVProgress.DataBind();
         }
         
 
